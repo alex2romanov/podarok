ed46cac868ae7543f2576c2cf4d681c23dcb4119
alex2romanov <pr.leha@yandex.ru>
2025-03-07 23:56:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const memoryBox = document.getElementById('memoryBox');
 const memories = {
     france: { text: "Поцелуй под Эйфелевой башней!", photo: "france.jpg" },
     penida: { text: "Ранний подъем и экстаз!", photo: "penida.jpg" },
-    elephant: { text: "До чего же большие гиганты!", photo: "elephants.jpg" },
+    elephant: { text: "До чего же большие гиганты!", photo: "elephant.jpg" },
     siargao: { text: "Побыли в раю на краю мира:)", photo: "siargao.jpg" },
     dragon: { text: "Увидели настоящего дракона и не были съедены!!", photo: "dragon.jpg" },
     danang: { text: "Оставалась сильной и непобедимой", photo: "danang.jpg" },
